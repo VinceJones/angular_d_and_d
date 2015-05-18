7464374bd7178b03ec99754d3a533e4865ec4f0b
Vince Jones <vincethebutcher@gmail.com>
2015-05-18 15:01:45 -0500

    initial commit

---
+++ client/scripts/app.js
@@ -0,0 +1,8 @@
+var App = angular.module('App', []);
+
+App.controller('GetStats', ["$scope", "$http", function($scope, $http){
+    $scope.stats = {};
+    $scope.allStats = [];
+
+    var get
+}]);
